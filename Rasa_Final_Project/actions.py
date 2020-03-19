0698d8793998be843fcabd2e9bbc5517c33d3ff2
Xiao Li <55390895+schawnlee@users.noreply.github.com>
2020-03-19 11:39:48 +0100

    V1.0 tested updated version with some random event

--- Rasa_Final_Project/actions.py
+++ Rasa_Final_Project/actions.py
@@ -4,6 +4,8 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
+import random
+
 class Room():
     def __init__(self, room_name="AN EMPTY ROOM", doors=[]):
         self.room_name = room_name
@@ -18,11 +20,11 @@ class Room():
 
 map = {
 
-    "OFFICE": [None, None, "OFFICE", "MEETING ROOM"],
-    "MEETING ROOM": [None, "OFFICE", "LAB", "CAFE"],
+    "OFFICE": [None, None, None, "MEETING ROOM"],
+    "MEETING ROOM": [None, "OFFICE", "LAB", "CAFETERIA"],
     "LAB": ["MEETING ROOM", None, "AUDITORIUM", None],
     "AUDITORIUM": ["LAB", None, "MAIN ENTRANCE", None],
-    "CAFE": [None, "MEETING", None, "MAIN ENTRANCE"]
+    "CAFETERIA": [None, "MEETING", None, "MAIN ENTRANCE"]
 
 }
 
@@ -44,6 +46,25 @@ def change_room(room_name, direction):
 
 
 
+
+def office_event():
+    paths = [" For emergency, there is nothing better then run STRAIGHT a way!", " Friday morning at 9:00, gathering for group picture infront of the entrance of the Cafeteria"]
+    path = random.choice(paths)
+    text = "Oh! This is Vlad's office, you find a note and it reads despite the bad light {}".format(path)
+    return text
+
+def cafe_event():
+    foods = ["SANDWICH", "BANANA", "CHOCKLADBAL", "NODDLE BOX", "COFFEE", "ORANGE JUICE", "WATER", "COOKIES",
+             "CINEMON BUN"]
+
+    text = "You found some {}".format(random.choice(foods)) + " and took it, since you are thirsty and hungry, and you feel better!"
+    return text
+
+def event_scary():
+    events = ["The light went suddenly out, it is completely dark", "These is some weird noise...", "You want to get out", "You are tired","You are missing your firends",
+              "You promise your self not ever to oversleep"]
+    text = random.choice(events)
+    return text
 
 from typing import Any, Text, Dict, List
 
@@ -67,7 +88,15 @@ class Update(Action):
             dispatcher.utter_message(template = "utter_goodbye")
         elif change_room(CURRENT_ROOM,DIRECTION):
             CURRENT_ROOM = change_room(CURRENT_ROOM,DIRECTION)
-            dispatcher.utter_message(text="You are now in {}".format(CURRENT_ROOM))
+            if random.random() < 0.5:
+                if CURRENT_ROOM == "OFFICE":
+                    dispatcher.utter_message(text=office_event())
+                if CURRENT_ROOM == "CAFETERIA":
+                    dispatcher.utter_message(text=cafe_event())
+                else:
+                    dispatcher.utter_message(text=event_scary())
+            dispatcher.utter_message(text="You are now in {}".format(CURRENT_ROOM) + " Where do you want to go next?")
+
         else:
             dispatcher.utter_message(template = "utter_no_way_out")
         return [SlotSet("room", CURRENT_ROOM), SlotSet("direction", None)]
--- Rasa_Final_Project/data/nlu.md
+++ Rasa_Final_Project/data/nlu.md
@@ -8,17 +8,18 @@
 
 ## intent: inform
 - [WEST](direction)
-- [OST](direction)
+- [EAST](direction)
 - [NORTH](direction)
 - [SOUTH](direction)
 - go [west](direction:WEST)
-- go [ost](direction:OST)
-- go [north](direction:NORTH)
 - go [east](direction:EAST)
+- go [north](direction:NORTH)
+- go [south](direction:SOUTH)
 - [west](direction:WEST)
-- [ost](direction:OST)
+- [south](direction:OST)
 - [north](direction:NORTH)
 - [east](direction:EAST)
+
 
 
 ## intent:affirm
